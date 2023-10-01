0216b3a9ff7bf176dc4c46b9552cb5cd9ee09f77
IshaanTctrl <dummy6123@outlook.com>
2023-09-30 17:18:19 -0700

    This program prints "Ishaan Taneja" to the console.

---
+++ Sep29HW2023.java
@@ -0,0 +1,30 @@
+public class Sep29HW2023 {
+    /*
+
+    4.
+    1 3 5
+    1 3 5 7 9 11 13 15
+    1 3 5 7 9 11 13 15 17 19 21 23 25
+
+    5.
+    1 2 3 4 5
+    1 2 3 4 5 6 7
+    1 2 3 4
+    number = 8
+
+    6.
+    three times two = 6
+    1 times three = 28
+    1 times 1 = 42
+    three times 1 = 2
+    1 times eight = 20
+
+    7.
+    whom and who like it
+    it and him like whom
+    whom and him like him
+    stu and boo like who
+    her and him like who
+
+    */
+}
